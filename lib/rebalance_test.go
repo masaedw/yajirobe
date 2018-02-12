d3bea35d66a5ea3908c975a34d686afed724a475
Masayuki Muto <masa.edw@gmail.com>
2018-02-12 14:37:14 +0900

    feat: round the RebalanceBuy() results to integers

--- lib/rebalance_test.go
+++ lib/rebalance_test.go
@@ -2,6 +2,52 @@ package yajirobe
 
 import "testing"
 
+func Test1(t *testing.T) {
+
+	funds := []*Fund{
+		&Fund{
+			Name:         "TestStock",
+			Code:         "1234",
+			Amount:       10,
+			AssetClass:   EmergingStocks,
+			CurrentPrice: 200,
+		},
+		&Fund{
+			Name:         "TestStock",
+			Code:         "5678",
+			Amount:       10,
+			AssetClass:   DomesticStocks,
+			CurrentPrice: 200,
+		},
+		&Fund{
+			Name:         "TestStock",
+			Code:         "90ab",
+			Amount:       10,
+			AssetClass:   InternationalStocks,
+			CurrentPrice: 600,
+		},
+	}
+
+	target := AllocationTarget{
+		EmergingStocks:      0.25,
+		DomesticStocks:      0.30,
+		InternationalStocks: 0.45,
+	}
+
+	a := NewAssetAllocation([]*Stock{}, funds, target)
+	result := a.RebalancingBuy(100)
+
+	assert := func(class AssetClass, expected float64) {
+		if result[class] != expected {
+			t.Errorf("%v expected %.2f but got %.2f", class, expected, result[class])
+		}
+	}
+
+	assert(EmergingStocks, 37)
+	assert(DomesticStocks, 63)
+	assert(InternationalStocks, 0)
+}
+
 func TestRound(t *testing.T) {
 	assert := func(expected, n float64) {
 		if round(n) != expected {
