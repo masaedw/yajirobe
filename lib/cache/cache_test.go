67355b28b9f4028c6a6a4da60ae0533f3845e0df
Masayuki Muto <masa.edw@gmail.com>
2018-06-02 10:55:41 +0900

    test: add tests

--- lib/cache/cache_test.go
+++ lib/cache/cache_test.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"go.uber.org/zap"
 )
 
 func tempDir(t *testing.T) string {
@@ -17,7 +19,8 @@ func tempDir(t *testing.T) string {
 }
 
 func tempDirInfoCache(tempDir string) Cache {
-	c, _ := NewFileCache(nil)
+	l, _ := zap.NewDevelopment()
+	c, _ := NewFileCache(l)
 	c.(*fileCache).path = tempDir
 	return c
 }
@@ -73,6 +76,46 @@ func TestFileSetGet(t *testing.T) {
 
 	if cache != json {
 		t.Fatalf("expected %v but got %v", json, cache)
+	}
+}
+
+func TestFileGetOrFind_NewKey(t *testing.T) {
+	tempDir := tempDir(t)
+	defer os.RemoveAll(tempDir)
+
+	c := tempDirInfoCache(tempDir)
+
+	json, err := c.GetOrFind("newkey", func(key string) (string, error) {
+		return "newkey_json", nil
+	})
+
+	if json != "newkey_json" {
+		t.Errorf("expected newkey_json but got %s", json)
+	}
+
+	if err != nil {
+		t.Errorf("error occured %v", err)
+	}
+}
+
+func TestFileGetOrFind_ExistsKey(t *testing.T) {
+	tempDir := tempDir(t)
+	defer os.RemoveAll(tempDir)
+
+	c := tempDirInfoCache(tempDir)
+	c.Set("existskey", "existskey_json")
+
+	json, err := c.GetOrFind("existskey", func(key string) (string, error) {
+		t.Errorf("finder unexpectedly called")
+		return "", nil
+	})
+
+	if json != "existskey_json" {
+		t.Errorf("expected existskey_json but got %s", json)
+	}
+
+	if err != nil {
+		t.Errorf("error occured %v", err)
 	}
 }
 
